1d28f41d7abfc96da33d3b49f6e143f31cac0dcd
rhoffenexitquitgit config --global user.name rhoffen <rebecca.hoffenberg@gmail.com>
2017-11-16 12:14:51 -0800

    JQuery added to animate navigation bar upon scrolling.

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  // Jumbotron background and text scroll every 4s with Slick plug-in
   $('.carousel').slick({
     autoplay: true,
     autoplaySpeed: 4000,
@@ -8,20 +9,21 @@ $(document).ready(function(){
     pauseOnHover: false,
   });
 
-  var $scrollDist = 0;
-  // console.log($scrollDist);
-  // $(window).scroll(function(){
-  //
-  //   var $scrollPsn = $(this).scrollTop();
-  //
-  //   if ($scrollPsn > $scrollDist + 50) {
-  //     var $navHeight = $('.navbar').css('height');
-  //    $('.navbar').animate({top: '-' + $navHeight},150);
-  //     $scrollDist = $scrollPsn;
-  //   } else if ($scrollDist > $scrollPsn + 50){
-  //     $('.navbar').animate({top: 0,150});
-  //     $scrollDist = $scrollPsn;
-  //   }
-  // });
+// Navigation header disappears as user scrolls down by 50px, and reappears as she scrolls up by 50px.
 
+  var $scrollDist = 0; //Initial position of scrollbar top.
+
+  $(window).scroll(function(){  //When user begins scrolling:
+
+    var $scrollPsn = $(this).scrollTop(); //Current scrollbar position.
+
+    if ($scrollPsn > $scrollDist + 50) { //If the current position exceeds the initial position by 50px, then...
+      var $navHeight = $('.navbar').css('height'); //Get the current height of the navbar.
+     $('.navbar').animate({top: '-' + $navHeight},150); //Change the position of top of the navbar to the negative of the navbar height over 150 ms, thus hiding it.
+      $scrollDist = $scrollPsn; //Update the initial position of the scrollbar height to the current position.
+    } else if ($scrollDist > $scrollPsn + 50){ //If the initial position of the scrollbar is greater than the current position by 50px, then the user is scrolling up so...
+      $('.navbar').animate({top: 0},150); //Replace the top of the navbar to 0 over 150ms, thus making it reappear.
+      $scrollDist = $scrollPsn; //Update the initial position of the scrollbar to the current position.
+    }
+  });
 });
