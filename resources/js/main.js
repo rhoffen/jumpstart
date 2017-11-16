1480d7bb00c74225c9afcfa5335b0ca3c2f4f4e0
rhoffenexitquitgit config --global user.name rhoffen <rebecca.hoffenberg@gmail.com>
2017-11-16 09:48:03 -0800

    Positioning of button in jumbotron section corrected.

--- resources/js/main.js
+++ resources/js/main.js
@@ -7,4 +7,21 @@ $(document).ready(function(){
     pauseOnFocus: false,
     pauseOnHover: false,
   });
+
+  var $scrollDist = 0;
+  // console.log($scrollDist);
+  // $(window).scroll(function(){
+  //
+  //   var $scrollPsn = $(this).scrollTop();
+  //
+  //   if ($scrollPsn > $scrollDist + 50) {
+  //     var $navHeight = $('.navbar').css('height');
+  //    $('.navbar').animate({top: '-' + $navHeight},150);
+  //     $scrollDist = $scrollPsn;
+  //   } else if ($scrollDist > $scrollPsn + 50){
+  //     $('.navbar').animate({top: 0,150});
+  //     $scrollDist = $scrollPsn;
+  //   }
+  // });
+
 });
